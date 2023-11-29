4ebd6dd3d2d3832314ddacfcd2e9b940ca563fe7
Jim Olson <jim.olson.456@gmail.com>
2023-11-29 00:20:39 -0600

    videoeditormovement

--- src/main/java/com/portfolio/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/portfolio/PortfolioApplication.java
@@ -3,17 +3,10 @@ package com.portfolio.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.InputStream;
-
 @SpringBootApplication
 public class PortfolioApplication {
 
-    public static void main(String[] args) {
-
-        InputStream mp4Stream = PortfolioApplication.class.getClassLoader().getResourceAsStream("6048474_Clouds Sky Mount Tamalpais Trees_By_Brad_Kremer_Artlist_HD.mp4");
-        SpringApplication.run(PortfolioApplication.class, args);
-
-
+    public static void main(String[] args) {SpringApplication.run(PortfolioApplication.class, args);
 
     }
 
